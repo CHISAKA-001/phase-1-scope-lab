39a3547b55f5622614148f3fae64989498fa8378
Edward Chisaka <edwardchisaka@Edwards-MacBook-Pro.local>
2022-08-19 03:49:07 -0700

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+//declare customerName to be bob in global scope
+var customerName = 'bob';
+
+//upperCaseCustomerName()
+function upperCaseCustomerName(){
+    return customerName =customerName.toUpperCase()
+}
+
+//setBestCustomer()
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+//overwriteBestCustomer()
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+//changeLeastFavoriteCustomer()
+const leastFavoriteCustomer = 'pre-nom';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "nom";
+}
